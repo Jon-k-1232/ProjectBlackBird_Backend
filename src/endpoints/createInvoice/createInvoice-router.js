04516d1c3756fd0e93af0af5e211d2605697308c
Jon Kimmel <Jonkimmel16@gmail.com>
2022-06-15 01:34:53 -0700

    create invoice updated

--- src/endpoints/createInvoice/createInvoice-router.js
+++ src/endpoints/createInvoice/createInvoice-router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const createInvoiceRouter = express.Router();
 const createInvoiceService = require('./createInvoice-service');
-const dayjs = require('dayjs');
 const createNewInvoice = require('./createInvoiceOrchestrator');
 const contactService = require('../contacts/contacts-service');
+const jsonParser = express.json();
+const { sanitizeFields } = require('../../utils');
+const { read } = require('pdfkit');
 
 // createInvoiceRouter.route('/download').get(async (req, res) => {
 //   // here we assigned the name to our downloaded file!
@@ -14,6 +16,9 @@ const contactService = require('../contacts/contacts-service');
 //   res.download(`${__dirname}/pdf_holder/${dayjs().format('YYYY-MM-DD')}/output.zip`);
 // });
 
+/**
+ * Multi invoice
+ */
 createInvoiceRouter.route('/createAllInvoices').get(async (req, res) => {
   const db = req.app.get('db');
 
@@ -22,6 +27,41 @@ createInvoiceRouter.route('/createAllInvoices').get(async (req, res) => {
   res.send({ newInvoices });
 });
 
+/**
+ * List of invoice ready to bill. User to select which to create
+ */
+createInvoiceRouter.route('/createInvoices/readyToBill').get(async (req, res) => {
+  const db = req.app.get('db');
+
+  const readyToBillContacts = await createInvoiceService.getReadyToBill(db);
+  res.send({
+    readyToBillContacts,
+    status: 200,
+  });
+});
+
+/**
+ * User selected invoices, create invoices
+ */
+createInvoiceRouter.route('/createInvoices/readyToBill/:list').post(jsonParser, async (req, res) => {
+  const db = req.app.get('db');
+  const list = req.params.list;
+
+  const sanitizedData = sanitizeFields({ list });
+  // Since sanitized, list is one giant string, must be separated at commas then converted into ints
+  const separatedList = sanitizedData.list.split(',');
+  const arrayOfIds = separatedList.map(item => Number(item));
+
+  const newInvoices = await Promise.all(arrayOfIds.map((contactRecord, i) => createNewInvoice(contactRecord, i, db)));
+  res.send({
+    newInvoices,
+    status: 200,
+  });
+});
+
+/**
+ * single invoice
+ */
 createInvoiceRouter.route('/createInvoice').get(async (req, res) => {
   const db = req.app.get('db');
   const readyToBillContacts = await contactService.getContactInfo(db, 245);
